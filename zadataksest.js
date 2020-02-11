ad00cca6ed4629b08d6b013dafa5efc15f3743f8
lmilica <l.milica91@gmail.com>
2020-02-11 23:44:16 +0100

    initiale JS commit

---
+++ zadataksest.js
@@ -0,0 +1,32 @@
+/*var c= 60;
+var f;
+var res = "";
+f= (9 * c/5) +32;
+res = c + "C is"  +  f +  " F";
+console.log(res);*/
+
+/*var a = 6;
+var b = 50;
+var res = "";
+
+if(a === 50 || b===50 || (a+b)===50){
+    res= "true";
+}else{
+    res= "-";
+}
+  console.log(res);*/
+
+  var a = 500;
+  var res = "";
+
+  if(a > 20 && a <100){
+      res = "20<=>100";
+  }else if(a > 100 && a < 400 ){
+      res = "100<=>400";
+  }else{
+      res = "-";
+  }
+   console.log(res);
+  
+  
+
