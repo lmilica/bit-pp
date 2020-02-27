504e80b0b55ffc5af247764827db38c9b03f7560
BIT Student <classroom@bgit.rs>
2020-02-27 14:09:06 +0100

    objects

---
+++ built.js
@@ -0,0 +1,52 @@
+//////////////1/////////////
+
+// var arr = [2, 4, 7, 11, -2, 1];
+// var newArr = [];
+// for (i = 0; i < arr.length; i++) {
+//     newArr.push(arr[i]);
+//     newArr.push(arr[i]);
+
+// }
+// console.log(newArr);
+///////222////
+// var arr = [8, 13, 8, 9, 12, 8, 1, 1, 4, 13];
+// function removeDuplicate(numbers) {
+//     var unique = [];
+
+//     for (var i = 0; i < numbers.length; i++) {
+//         if (unique.indexOf(numbers[i]) === -1) {
+//             unique.push(numbers[i]);
+//         }
+//     }
+
+//     return unique.sort(function (a, b) {
+//         return a - b;
+//     })
+// }
+
+// console.log(removeDuplicate(arr));
+////3333////
+///a.///
+// var arr = [1, 2, 9, 2, 1];
+
+// function oddOrEven(numbers) {
+
+//     if (numbers.length % 2 === 1) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+
+// console.log(oddOrEven(arr));
+
+////b.////
+
+function oddOrEven(numbers){
+    if(numbers.length % 2 === 0){
+        return "Error";
+    }else{
+        
+    }
+}
