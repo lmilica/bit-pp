8bc00295ff432d2d86f4713e2c362f35ecce4f34
BIT Student <classroom@bgit.rs>
2020-03-03 18:02:21 +0100

    Functions6

---
+++ scope.js
@@ -0,0 +1,56 @@
+'use strict';
+/////func6task2////
+// var res = (function () {
+//     var text = ['a', 'b', 'c'];
+//     var numbers = [1, 2, 3];
+//     var arr = [];
+
+//     for (var i = 0; i < text.length; i++) {
+//         arr.push(text[i]);
+//         arr.push(numbers[i]);
+//     }
+
+//     return arr;
+// })();
+
+// console.log(res);
+
+
+////func6task3///
+
+// (function () {
+//     var numbers = [1, 2, 3, 4, 5, 6];
+//     var k = 3;
+//     var firstRes = numbers.slice(0, k);
+//     var secondRes = numbers.slice(k, numbers.length);
+//     var finalRes = secondRes.concat(firstRes);
+//     console.log(finalRes);
+
+
+// })();
+
+////func6task4///
+
+// (function () {
+//     var arr = [];
+//     var number = 123543;
+//     var str = number.toString();
+//     for (var i = 0; i < str.length; i++) {
+//         arr.push(parseInt(str[i]));
+
+//     }
+//     console.log(arr);
+
+// })();
+
+
+// ////func6task5///
+
+// (function() {
+//     var a=12;
+//     for (i=0; i<a;i++){
+//         for(j=0;j<a;j++){
+
+//         }
+//     }
+// })();
